572d7ea9a337831b5de1046271f1cc0c5138eb5c
Mu'taz <mutazalaasalah@gmail.com>
2020-11-24 12:53:37 +0200

    handle responsive for header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React ,{useState} from "react";
 import img from "./pic.svg";
-
+import burgerMenu from "./menuu.svg";
 export const Header = () => {
+  const [opened,setOpened]=useState(false)
   return (
     <header>
-      <nav className="mr-16 ml-32 mt-4 flex justify-between items-center">
+      <nav>
+        <div className="lg:mr-16 mr-4 lg:ml-32 ml-4 lg:mt-4 mt-2 flex justify-between items-center">
         <h1 className="font-bold">
           <a href="#">
             <svg
@@ -30,35 +32,73 @@ export const Header = () => {
             Trafalgar
           </a>
         </h1>
-        <ul className="font-bold">
-          <li className="inline-block pr-2 pl-2">
-            <a href="#">Home</a>
-          </li>
-          <li className="inline-block pr-2 pl-2">
-            <a href="#">Find a doctor</a>
-          </li>
-          <li className="inline-block pr-2 pl-2">
-            <a href="#">Apps</a>
-          </li>
-          <li className="inline-block pr-2 pl-2">
-            <a href="#">Testimonials</a>
-          </li>
-          <li className="inline-block pr-2 pl-2">
-            <a href="#">About us</a>
-          </li>
+        <ul className="font-bold lg:flex justify-center items-center">
+          <div className="font-bold lg:flex hidden">
+            <li className=" pr-2 pl-2">
+              <a href="#">Home</a>
+            </li>
+            <li className="pr-2 pl-2">
+              <a href="#">Find a doctor</a>
+            </li>
+            <li className="pr-2 pl-2">
+              <a href="#">Apps</a>
+            </li>
+            <li className=" pr-2 pl-2">
+              <a href="#">Testimonials</a>
+            </li>
+            <li className=" pr-2 pl-2">
+              <a href="#">About us</a>
+            </li>
+          </div>
+          <div>
+            <li className="pr-2 pl-2 lg:hidden cursor-pointer">
+              <button onClick={()=>setOpened(!opened)}>
+                <img src={burgerMenu} alt="" />
+              </button>
+            </li>
+          </div>
         </ul>
-      </nav>
-      <div className="mt-16 mr-16 ml-32 grid grid-cols-2 items-center">
-        <div>
-          <h1 className="font-bold text-4xl">Virtual healthcare<br/> for you</h1>
-          <p className="mt-4 text-gray-500">
-            Trafalgar provides progressive, and affordable<br /> healthcare,
-            accessible on mobile and<br/> online for everyone
-          </p>
-          <button className="rounded-full mt-4 bg-blue-500 text-white font-bold p-2">Consult today</button>
         </div>
+        <div className={`w-full font-bold pt-2 shadow-xl	 lg:hidden  ${!opened?"invisible":null}`}>
+        <ul>
+            <li className=" pr-2 pl-4 cursor-pointer w-full text-gray-500 hover:text-black">
+              <a href="#">Home</a>
+            </li>
+            <hr />
+            <li className="pr-2 pl-4 cursor-pointer w-full text-gray-500 hover:text-black">
+              <a href="#">Find a doctor</a>
+            </li>
+            <hr />
+            <li className="pr-2 pl-4 cursor-pointer w-full text-gray-500 hover:text-black">
+              <a href="#">Apps</a>
+            </li>
+            <hr />
+            <li className=" pr-2 pl-4 cursor-pointer w-full text-gray-500 hover:text-black">
+              <a href="#">Testimonials</a>
+            </li>
+            <hr />
+            <li className=" pr-2 pl-4 cursor-pointer w-full text-gray-500 hover:text-black">
+              <a href="#">About us</a>
+            </li>
+            <hr />
+        </ul>
+        </div>
+      </nav>
+      <div className=" lg:mt-16 mr-16 lg:ml-32 ml-4 grid grid-cols-1 lg:grid-cols-2 items-center">
         <div>
-            <img src={img} alt="" />
+          <h1 className="font-bold text-4xl lg:text-5xl  lg:w-3/4">
+            Virtual healthcare for you
+          </h1>
+          <p className="mt-4 text-gray-500 lg:w-3/4 ">
+            Trafalgar provides progressive, and affordable healthcare,
+            accessible on mobile and online for everyone
+          </p>
+          <button className="rounded-full mt-4 bg-blue-1000 text-white font-bold p-2">
+            Consult today
+          </button>
+        </div>
+        <div className="hidden lg:block">
+          <img src={img} alt="" />
         </div>
       </div>
     </header>
