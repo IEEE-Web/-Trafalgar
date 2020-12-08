2f417bbd0f90da835ca078646e0c48d54a8616e1
asmaafayed02 <asmaafayed02@gmail.com>
2020-12-09 01:06:56 +0200

    handling error and loading

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -6,7 +6,7 @@ export const Services =()=>{
         <section style={{backgroundImage: "URL('./Images/serviceSvgs/servicesBackground.svg')"}} className='bg-cover'>
               <div className= 'container mx-auto px-24'>
                 <div className='text-center'>
-                    <h1 className='text-4xl font-bold pb-4'>Our services</h1>
+                    <h2 className='text-4xl font-bold pb-4'>Our services</h2>
                     <div className='flex justify-center'>
                     <hr className='w-8 border border-black '/>
                     </div>
@@ -18,7 +18,7 @@ export const Services =()=>{
                 <div className='flex justify-center'>
                     <button className='border md:px-10 py-2 m-12 rounded-full font-bold border-blue-500 text-blue-500 cursor-pointer tracking-wider hover:bg-blue-500 hover:text-white transition ease-in-out duration-700'>
                         Learn more
-                        </button>
+                    </button>
                 </div>
                 </div>
         </section>
--- src/components/Services/ourServices.js
+++ src/components/Services/ourServices.js
@@ -2,26 +2,45 @@ import React, { Component } from 'react';
 import axios from 'axios';
 class OurServices extends Component {
     state = { 
-        services: []
+        isLoaded: false,
+        error: null,
+        ourservices: []
      }
     componentDidMount(){//fetch data form json server
         axios.get("http://localhost:3000/services")
          .then(response =>{
             console.log(response.data)
             this.setState({//updeate the state
-                services: response.data
+                isLoaded: true,
+                ourservices: response.data
             })
-        })
+        },
+        (error)=>{
+            this.setState({
+                isLoaded: true,
+                error
+            })
+        }
+        )
     }
+
     render() { 
+        const {isLoaded, error, ourservices} = this.state;
+        if(error){//handling error
+        return <div>Error: {error.message}</div>
+
+        }else if(!isLoaded){//handling loading
+            return <div className='font-extrabold text-center shadow-lg p-10 border '>Loading...</div>
+
+        }else{
         return (//maping to show our data
-            <div className='grid lg:grid-cols-3 gap-8 mt-6'>
-                {this.state.services.map(serviceItem =>{
+            <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-8 mt-6'>
+                {ourservices.map(serviceItem =>{
                     return(
                         <div key={serviceItem.service_id} className='p-6 rounded-xl overflow-hidden shadow-lg hover:shadow-xl bg-white grid grid-rows-2 transition ease-out duration-500'>
                             <img src= {serviceItem.service_icon} className='' alt='service icon '/>
                             <div>
-                            <h3 className='font-bold py-4'>{serviceItem.service_title}</h3>
+                            <p className='font-bold py-4'>{serviceItem.service_title}</p>
                             <p className='font-light text-gray-500'>{serviceItem.service_description}</p>
                             </div>
                         </div>
@@ -31,6 +50,7 @@ class OurServices extends Component {
             </div>
           );
     }
+}
 }
  
 export default OurServices;
